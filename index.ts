d090b936ce3ba2040251d23f14fb6036426642cb
Alejandro <ja.jimenezrosa@gmail.com>
2020-02-07 07:31:20 -0400

    levantar la base de datos MongoDB y Conectar nuestra aplicacion....

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import Server from './classes/server';
 import userRourtes from './routes/usuarios';
-
+import mongoose, { Mongoose } from 'mongoose';
 
 
 const server = new Server();
 
 //Rutas de mi aplicacion
 server.app.use( '/user', userRourtes );
+
+
+//Conectar DB
+mongoose.connect('mongodb://localhost:27017/fotosgrams',
+               { useUnifiedTopology: true, useNewUrlParser: true , useCreateIndex: true}, 
+                            (err) => {
+                              if ( err ) throw err;
+                              console.log('Base de datos ONLINE!');
+                            })
+         
 
 //Levantar Express
 server.start(  () => {
